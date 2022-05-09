9e0a8fcaf6ef5cff0e302d4b63b62e3701bdf791
Nazeer Basha Shaik <farru@192.168.1.2>
2022-05-09 11:01:12 +0530

    Getting error in local

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -1,1 +1,11 @@
-// Write your code here
+import {StyledHeader , StyledDiv} from "./styledComponents";
+
+const ReadMore = () => {
+    return (
+        <StyledDiv>
+            <StyledHeader as="h2">React Hooks</StyledHeader>
+        </StyledDiv>
+    );
+}
+
+export default ReadMore;
